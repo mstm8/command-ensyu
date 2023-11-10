913351e9c9e184461c9aa489a8a9a56b07c666bb
masatomo <m.uemura.sys23@morijyobi.ac.jp>
2023-11-10 09:41:47 +0900

    Main.javaの名前をBmi.javaに変更

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         for (int i = 1; i <= 2; i++){
